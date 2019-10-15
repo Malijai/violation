4fbb514f805f82f5df9a65144313016b453a60f6
Malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2019-10-15 09:17:58 -0400

    Correction de tous les fichiers d'url (mise en conformite Django2)

--- urls.py
+++ urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from .views import rechercher
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^$', rechercher, name='Rechercherviol'),
-    url(r'listevcc.html', TemplateView.as_view(template_name="listevcc.html"), name='listevccs'),
+    path('', rechercher, name='Rechercherviol'),
+    path('listevcc.html', TemplateView.as_view(template_name="listevcc.html"), name='listevccs'),
 ]
 
